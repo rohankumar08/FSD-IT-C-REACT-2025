f940fe2feacfe13911c3f71a7994830ebad19fbc
ROHAN KUMAR <rohansahu1234ssm@gmail.com>
2025-11-24 12:27:48 +0530

    useEffect

--- React Work/ShoppingCart/src/component/Dashboard.jsx
+++ React Work/ShoppingCart/src/component/Dashboard.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Dashboard() {
+
+  const[counter,setCounter]=useState(10);
+
+  const [pointer,setPointer]=useState(50);
+  useEffect(()=>{
+    console.log(counter)
+    console.log(pointer)
+  },[counter,pointer])
   return (
-    <div>Dashboard</div>
+    <div>
+      <div>
+        <h2>counter value={counter}</h2>
+        <h2>Pointer Value={pointer}</h2>
+      </div>
+      <button onClick={()=>setCounter(counter+20)}>Counter</button>
+      <button onClick={()=>setPointer(pointer+1)}>Pointer</button>
+    </div>
   )
 }
 
